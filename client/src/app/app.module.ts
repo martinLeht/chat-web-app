2308e569babefdec3a22500eca3dadfb20d7fe53
Martin Lehtomaa <martin.lehtomaa@gmail.com>
2019-07-21 18:01:10 +0300

    modified .gitignore file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SidebarNavComponent } from './components/sidebar-nav/sidebar-nav.component';
+import { MainSectionComponent } from './components/main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarNavComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
