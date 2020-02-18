a4c6d1d9f51932e4740e071295d63b2e39335af4
N6397 <N6397@student.jamk.fi>
2020-02-18 21:49:12 +0200

    Made some modifications for the UI to work better on scaling

--- client/src/app/components/chat-window/chat-window.component.ts
+++ client/src/app/components/chat-window/chat-window.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatWindowComponent implements OnInit {
 
-  constructor() { }
+  private messages: Array<object>;
+  isChatting = true;
+  loggedUser = "Saitama";
+
+  constructor() {
+    this.messages = [
+      {
+        "user": "Saitama",
+        "message": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Frodo",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Saitama",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Frodo",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Saitama",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Frodo",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      },
+      {
+        "user": "Saitama",
+        "message": "JHIII mayn get this bread mans. Maito on semmosta.AINA!?",
+        "time": new Date().toLocaleDateString()
+      }
+    ]; 
+  }
 
   ngOnInit() {
+  }
+
+  getMessages() {
+    return this.messages
+  }
+
+  sendMessage() {
+    this.messages.push(
+      {
+        "user": "Saitama",
+        "message": "UFFF new MEssage man.",
+        "time": new Date().toLocaleDateString()
+      }
+    ) 
   }
 
 }
